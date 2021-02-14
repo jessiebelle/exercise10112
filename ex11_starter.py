763586a8c23965577a28973910d05264fcbbaaa2
Rafika <RTuremis@hotmail.co.uk>
2021-02-14 18:52:34 +0000

    Exercise 11 completed

--- ex11_starter.py
+++ ex11_starter.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python
 
 Belgium = 'Belgium,10445852,Brussels,737966,Europe,1830,Euro,Catholicism,Dutch,French,German'
-
+print('-' * len(Belgium))
+print(Belgium.replace(',', ':'))
+x = int(Belgium[8:16])
+print(x)
+y = int(Belgium[26:32])
+print(y)
+z=x+y
+print(z)
