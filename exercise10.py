646cca4361b5f093d17d119a93947a00437bc955
Jessie Auguste <jessieauguste@gmail.com>
2021-02-14 18:27:26 +0000

    finished exercise 10git add .git add .

--- exercise10.py
+++ exercise10.py
@@ -3,15 +3,20 @@
 # restrict number of attempts to 3
 # output message for success
 # output message for failure
+import getpass
 correct_pin = '1234'
 max_tries = 0
-supplied_pin = input("Enter your PIN: ")
-if len(supplied_pin) > 4:
-    print("Error! Only 4 numbers allowed ")
+supplied_pin = getpass.getpass("Enter your PIN: ")
+# if len(supplied_pin) !=4:
+#     print("Error! Only 4 numbers allowed ")
+# when above happens, we do not need the below error message
 
 while supplied_pin != correct_pin and max_tries <2:
+    if len(supplied_pin) != 4:
+        print("Error! 4 numbers required")
     print("Wrong number, please try again")
-    supplied_pin = input("Enter your PIN: ")
+    print('You have ' + (str(2 - max_tries)) + ' attempts left')
+    supplied_pin = getpass.getpass("Enter your PIN: ")
     max_tries = max_tries +1
     if max_tries == 2:
         print("Run out of tries!")
